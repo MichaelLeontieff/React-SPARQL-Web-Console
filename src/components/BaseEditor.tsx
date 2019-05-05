817ed7ea6f889be23386bf9e06fe8d357deec4bf
MichaelLeontieff <michael@leontieff.net>
2019-05-05 20:31:42 +1000

    enabled included sparql support

--- src/components/BaseEditor.tsx
+++ src/components/BaseEditor.tsx
@@ -3,6 +3,37 @@ import React, { Component } from 'react';
 import CodeMirror from "codemirror";
 
 import 'codemirror/lib/codemirror.css';
+import 'codemirror/mode/sparql/sparql';
+
+
+/**
+ * TODO: 
+ *  - Expanded tokenizer to accomodate .rql or whatever language keywords like "invoke" come from
+ */
+export const PLACEHOLDER = `
+  PREFIX a: <http://www.w3.org/2000/10/annotation-ns#>
+  PREFIX dc: <http://purl.org/dc/elements/1.1/>
+  PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+  PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+
+  # Comment!
+
+  SELECT ?given ?family
+  WHERE {
+    {
+      ?annot a:annotates <http://www.w3.org/TR/rdf-sparql-query/> .
+      ?annot dc:creator ?c .
+      OPTIONAL {?c foaf:givenName ?given ;
+                  foaf:familyName ?family }
+    } UNION {
+      ?c !foaf:knows/foaf:knows? ?thing.
+      ?thing rdfs
+    } MINUS {
+      ?thing rdfs:label "剛柔流"@jp
+    }
+    FILTER isBlank(?c)
+  }
+`;
 
 class BaseEditor extends Component {
 
@@ -18,14 +49,14 @@ class BaseEditor extends Component {
   }
 
   componentDidMount() {
-    // instantiate editor
+    // instance editor
     this.container = document.querySelector("#editor-container");
     this.editor = CodeMirror(this.container, { 
-      value: "testing",
-      mode: "javascript",
+      value: PLACEHOLDER,
+      mode: "application/sparql-query",
       lineNumbers: true,
-      theme: "eclipse"
-    });
+      matchBrackets: true
+    } as any);
   }
 }
 
